7ad346dce6c5a241cf44e92d81d6ca3033fa7e9a
earvin <earvin@debian>
2015-10-18 22:37:07 +0200

    assets favicon

--- server.js
+++ server.js
@@ -4,11 +4,13 @@
     var express = require('express'),
 	app = express(),
 	fs = require('fs'),
+	favicon = require('serve-favicon'),
 	port = process.env.PORT || 10002;
 
     
     app.use(express.static(__dirname + "/public"));
-
+    app.use(favicon(__dirname + '/public/assets/favicon.ico'));
+    
     app.listen(port);
     console.log("Server running on " + port);
     
