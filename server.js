74e26d56af35fbe10b4c7ac5497fa3fb263c3470
earvin <earvin@debian>
2015-10-21 19:57:48 +0200

    fav

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
 	    app = express(),
 	    port = process.env.PORT || 10002;
 
-	app.use(favicon(__dirname + '/public/img/favicon.ico'));
+	app.use(express.static(__dirname + '/public'));
 	app.listen(port);
 	console.log("Server running on " + port);	
     }
