9a842d8319d7e4e093b5777cc658981ddbb5446d
EarvinBevolta <e.kayonga@bevolta.com>
2016-02-04 14:24:55 +0100

    Ejs

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-(function(){
-    'use strict';
+(function() {
+  'use strict';
 
-    var express = require('express'),
-	app = express(),
-	fs = require('fs'),
-	favicon = require('serve-favicon'),
-	port = process.env.PORT || 10002;
+  const express = require('express'),
+    app = express(),
+    fs = require('fs'),
+    favicon = require('serve-favicon'),
+    port = process.env.PORT || 10002;
 
-    
-    app.use(express.static(__dirname + "/public"));
-    app.use(favicon(__dirname + '/public/assets/favicon.ico'));
+  app.set('view engine', 'ejs');
+  app.use(express.static(__dirname + "/public"));
+  app.use(favicon(__dirname + '/public/assets/favicon.ico'));
 
-    app.get('*', function(req, res){
-	res.send('TA RAce', 404);
-    });
-    
-    app.listen(port);
-    console.log("Server running on " + port);
-    
-}
-)();
+  app.all('*', function(req, res) {
+    res.render('error');
+  });
+
+  app.listen(port);
+  console.log("Server running on " + port);
+
+})();
