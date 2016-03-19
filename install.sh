a464bf253334a883bc5bf485b59f7c0e59f9faf2
EarvinKayonga <demoneartin@gmail.com>
2016-03-20 00:10:09 +0100

    Linux

--- install.sh
+++
@@ -1,8 +0,0 @@
-service apache2 stop;
-apt-get install build-essential nodejs-legacy npm ruby-full nginx;
-npm install -g express bower forever;
-npm install && (cd public && bower install);
-chmod 777 launch.sh;
-./launch.sh;
-mv earvinkayonga.com /etc/nginx/sites-enabled/default
-service nginx restart
--- launch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Launching the server";
-forever --minUptime 2000 --spinSleepTime 2000 -al access.log -o out.log -e err.log start server.js
