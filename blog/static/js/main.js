4bbb334d18ba18961bcfe56204b6b27b005131ea
EarvinKayonga <demoneartin@gmail.com>
2016-10-06 15:28:26 +0200

    hamburger menu

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -12,3 +12,9 @@ jQuery(document).ready(function(e) {
 });
 
 
+function toggledisplay(elementID){
+       (
+         function(style) {
+           style.display = style.display === 'block'? 'none' : 'block';
+       })(document.getElementById(elementID).style);
+}
